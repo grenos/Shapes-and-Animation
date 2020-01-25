21bd5348d81682c394148c0daf817f716fff4627
grenos <Vasilis.green@gmail.com>
2020-01-25 13:26:31 +0100

    Initial Commit

---
+++ Shapes and Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shapes and Animation
+//
+//  Created by Vasilis Green on 25/1/20.
+//  Copyright © 2020 Vasilis Green. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
